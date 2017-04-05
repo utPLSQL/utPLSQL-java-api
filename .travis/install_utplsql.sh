f4a9810cc1e71af1bddc2c66a73c9bcd702e39b9
Vinicius Avellar <amvinicius@gmail.com>
2017-04-05 18:55:10 -0300

    Initial travis configuration

---
+++ .travis/install_utplsql.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Download the specified version of utPLSQL.
+curl -L -O "https://github.com/utPLSQL/utPLSQL/releases/download/$UTPLSQL_VERSION/$UTPLSQL_FILE.tar.gz"
+
+# Create a temporary install script.
+cat > install.sh.tmp <<EOF
+tar -xzf $UTPLSQL_FILE.tar.gz
+rm $UTPLSQL_FILE.tar.gz
+
+cd /$UTPLSQL_FILE/source
+
+sqlplus -S -L system/oracle@//$CONNECTION_STR @install_headless.sql
+sqlplus -S -L system/oracle@//$CONNECTION_STR @install.sql ut3
+EOF
+
+# Copy utPLSQL files to the container and install it.
+docker cp ./$UTPLSQL_FILE.tar.gz $ORACLE_VERSION:/$UTPLSQL_FILE.tar.gz
+docker cp ./install.sh.tmp $ORACLE_VERSION:/install.sh
+
+# Remove temporary files.
+rm $UTPLSQL_FILE.tar.gz
+rm install.sh.tmp
+
+# Execute the utPLSQL installation inside the container.
+docker exec $ORACLE_VERSION bash install.sh
