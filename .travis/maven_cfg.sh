9162ffac9bfc09ea4d5aff6aef9b6ba38a246c37
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-04-07 14:49:48 -0300

    Using http-wagon recommended by oracle

--- .travis/maven_cfg.sh
+++ .travis/maven_cfg.sh
@@ -2,9 +2,10 @@
 set -ev
 cd $(dirname $(readlink -f $0))
 
-if [ -d $MAVEN_HOME ]; then
-    echo "Using cached maven install..."
-    $MAVEN_HOME/bin/mvn -v
+mavenSettings=$HOME/.m2/settings.xml
+
+if [ -d $mavenSettings ]; then
+    echo "Using cached maven user config..."
     exit 0
 fi
 
@@ -15,14 +16,17 @@ fi
 
 # If not cached, download and install maven.
 # Then create the settings file, with username/password for oracle server.
-curl -L -O "http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
-tar -xzf apache-maven-3.3.9-bin.tar.gz
-mv apache-maven-3.3.9 $MAVEN_HOME
-rm apache-maven-3.3.9-bin.tar.gz
+# curl -L -O "http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+# tar -xzf apache-maven-3.3.9-bin.tar.gz
+# mv apache-maven-3.3.9 $MAVEN_HOME
+# rm apache-maven-3.3.9-bin.tar.gz
 
-settingsFile=$MAVEN_HOME/conf/settings.xml
-cp settings.tmpl.xml $settingsFile
-sed -i -e "s|###USERNAME###|$ORACLE_OTN_USER|g" $settingsFile
-sed -i -e "s|###PASSWORD###|$ORACLE_OTN_PASSWORD|g" $settingsFile
+curl -L -O "http://central.maven.org/maven2/org/apache/maven/wagon/wagon-http/2.8/wagon-http-2.8-shaded.jar"
+sudo mv wagon-http-2.8-shaded.jar $MAVEN_HOME/lib/ext/
 
-$MAVEN_HOME/bin/mvn -v
+# MAVEN_CFG=$MAVEN_HOME/conf/settings.xml
+cp settings.tmpl.xml $mavenSettings
+sed -i -e "s|###USERNAME###|$ORACLE_OTN_USER|g" $mavenSettings
+sed -i -e "s|###PASSWORD###|$ORACLE_OTN_PASSWORD|g" $mavenSettings
+
+# $MAVEN_HOME/bin/mvn -v
