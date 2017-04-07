45c88d061b13d358bae5624aab0e4256c1f2f118
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-04-07 14:22:43 -0300

    Trying to use downloaded maven version

--- .travis/maven_cfg.sh
+++ .travis/maven_cfg.sh
@@ -2,19 +2,27 @@
 set -ev
 cd $(dirname $(readlink -f $0))
 
+if [ -d $MAVEN_HOME ]; then
+    echo "Using cached maven install..."
+    $MAVEN_HOME/bin/mvn -v
+    exit 0
+fi
+
 if [ "$ORACLE_OTN_USER" == "" ] || [ "$ORACLE_OTN_PASSWORD" == "" ]; then
     echo "Oracle OTN username/password not specified."
     exit 1
 fi
 
-# Copy the maven settings file to the right place, and set the username/password for oracle maven server.
-# cp settings.tmpl.xml $MAVEN_SETTINGS
-# sed -i -e "s|###USERNAME###|$ORACLE_OTN_USER|g" $MAVEN_SETTINGS
-# sed -i -e "s|###PASSWORD###|$ORACLE_OTN_PASSWORD|g" $MAVEN_SETTINGS
+# If not cached, download and install maven.
+# Then create the settings file, with username/password for oracle server.
+curl -L -O "http://mirror.nbtelecom.com.br/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz"
+tar -xzf apache-maven-3.3.9-bin.tar.gz
+mv apache-maven-3.3.9 $MAVEN_HOME
+rm apache-maven-3.3.9-bin.tar.gz
 
-echo $M2_HOME
-echo $JAVA_HOME
-ls -la $M2_HOME
-ls -la $MAVEN_HOME
+settingsFile=$MAVEN_HOME/conf/settings.xml
+cp settings.tmpl.xml $settingsFile
+sed -i -e "s|###USERNAME###|$ORACLE_OTN_USER|g" $settingsFile
+sed -i -e "s|###PASSWORD###|$ORACLE_OTN_PASSWORD|g" $settingsFile
 
-sudo find / -type d -iname "maven"
+$MAVEN_HOME/bin/mvn -v
