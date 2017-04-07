e10900996423310c70f6f193a66df7efd24d6e42
Vinicius Avellar <amvinicius@gmail.com>
2017-04-06 22:37:17 -0300

    Trying settings.xml with env vars

--- .travis/maven_cfg.sh
+++ .travis/maven_cfg.sh
@@ -8,6 +8,6 @@ if [ "$ORACLE_OTN_USER" == "" ] || [ "$ORACLE_OTN_PASSWORD" == "" ]; then
 fi
 
 # Copy the maven settings file to the right place, and set the username/password for oracle maven server.
-cp settings.tmpl.xml $MAVEN_SETTINGS
-sed -i -e "s|###USERNAME###|$ORACLE_OTN_USER|g" $MAVEN_SETTINGS
-sed -i -e "s|###PASSWORD###|$ORACLE_OTN_PASSWORD|g" $MAVEN_SETTINGS
+# cp settings.tmpl.xml $MAVEN_SETTINGS
+# sed -i -e "s|###USERNAME###|$ORACLE_OTN_USER|g" $MAVEN_SETTINGS
+# sed -i -e "s|###PASSWORD###|$ORACLE_OTN_PASSWORD|g" $MAVEN_SETTINGS
