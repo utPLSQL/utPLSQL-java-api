13c0b2c42db092d67898918180c5245020c31937
Vinicius Avellar Moreira <amvinicius@gmail.com>
2017-05-18 08:37:33 -0300

    Fix typo

--- src/main/java/io/github/utplsql/api/types/CustomTypes.java
+++ src/main/java/io/github/utplsql/api/types/CustomTypes.java
@@ -1,14 +1,13 @@
 package io.github.utplsql.api.types;
 
 /**
- * Created by Vinicius on 13/04/2017.
- * DBHelper custom data types.
+ * Database custom data types.
  */
 public enum CustomTypes {
     // Object names must be upper case.
     UT_DOCUMENTATION_REPORTER("UT_DOCUMENTATION_REPORTER"),
     UT_COVERAGE_HTML_REPORTER("UT_COVERAGE_HTML_REPORTER"),
-    UT_VARCHAF2_LIST("UT_VARCHAR2_LIST");
+    UT_VARCHAR2_LIST("UT_VARCHAR2_LIST");
 
     private String typeName;
 
