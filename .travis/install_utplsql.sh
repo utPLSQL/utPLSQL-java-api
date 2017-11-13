a719473f1ceb6d8e10eab5637071c5945b1a9c69
Jacek <jgebal@users.noreply.github.com>
2017-11-13 07:19:02 +0000

    Added build matrix for Travis API will now be executed against multiple releases of utPLSQL

--- .travis/install_utplsql.sh
+++ .travis/install_utplsql.sh
@@ -3,8 +3,6 @@ set -ev
 cd $(dirname $(readlink -f $0))
 
 # Download the specified version of utPLSQL.
-UTPLSQL_VERSION="v3.0.4"
-UTPLSQL_FILE="utPLSQL"
 curl -L -O "https://github.com/utPLSQL/utPLSQL/releases/download/$UTPLSQL_VERSION/$UTPLSQL_FILE.tar.gz"
 
 # Download develop branch of utPLSQL.
