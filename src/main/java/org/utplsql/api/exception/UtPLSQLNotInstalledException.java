f63774a341aac4382097d1a754b6ad9833d5b216
pesse <derpesse@gmail.com>
2017-11-17 09:30:46 +0100

    Introduce special Exception for when utPLSQL is not installed

---
+++ src/main/java/org/utplsql/api/exception/UtPLSQLNotInstalledException.java
@@ -0,0 +1,16 @@
+package org.utplsql.api.exception;
+
+import java.sql.SQLException;
+
+/** Exception to track when utPLSQL framework is not installed or accessible on the used database
+ *
+ * @author pesse
+ */
+public class UtPLSQLNotInstalledException extends SQLException {
+
+    public static final int ERROR_CODE = 904;
+
+    public UtPLSQLNotInstalledException( SQLException cause ) {
+        super("utPLSQL framework is not installed on your database or not accessable to the user you are connected with", cause);
+    }
+}
