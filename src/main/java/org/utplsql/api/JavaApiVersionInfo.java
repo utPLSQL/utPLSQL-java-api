7256c6cef8976477abdfe6ae290fd81302a4c761
pesse <derpesse@gmail.com>
2018-05-23 09:19:59 +0200

    Try adding maven and travis build number

---
+++ src/main/java/org/utplsql/api/JavaApiVersionInfo.java
@@ -0,0 +1,18 @@
+package org.utplsql.api;
+
+/** This class is getting updated automatically by the build process.
+ * Please do not update its constants manually cause they will be overwritten.
+ *
+ * @author pesse
+ */
+public class JavaApiVersionInfo {
+
+    private static final String BUILD_NO = "123";
+    private static final String MAVEN_PROJECT_NAME = "utPLSQL-java-api";
+    private static final String MAVEN_PROJECT_VERSION = "3.1.1-SNAPSHOT";
+
+    public static String getVersion() {
+        return MAVEN_PROJECT_VERSION + "." + BUILD_NO;
+    }
+
+}
