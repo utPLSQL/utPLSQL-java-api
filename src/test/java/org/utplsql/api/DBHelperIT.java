7a38610974927f27ac2e3ade2d5e77138dc01b05
pesse <derpesse@gmail.com>
2018-06-05 21:24:04 +0200

    New method to check Oracle Database version

--- src/test/java/org/utplsql/api/DBHelperIT.java
+++ src/test/java/org/utplsql/api/DBHelperIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class DBHelperIT extends AbstractDatabaseTest {
 
@@ -12,6 +13,12 @@ public class DBHelperIT extends AbstractDatabaseTest {
     public void getFrameworkVersion() throws SQLException {
         Version v = DBHelper.getDatabaseFrameworkVersion(getConnection());
         assertEquals(true, v.isValid());
+    }
+
+    @Test
+    public void getOracleDatabaseVersion() throws SQLException {
+        String databaseVersion = DBHelper.getOracleDatabaseVersion(getConnection());
+        assertNotNull(databaseVersion);
     }
 
 }
