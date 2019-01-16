41f0a479c0e7450ada6d4286e9e80815839aaca3
pesse <derpesse@gmail.com>
2019-01-16 07:32:57 +0100

    Reporter-name should always be a simple SQL name

--- src/main/java/org/utplsql/api/outputBuffer/OutputBufferProvider.java
+++ src/main/java/org/utplsql/api/outputBuffer/OutputBufferProvider.java
@@ -53,7 +53,7 @@ public class OutputBufferProvider {
                 "   begin " +
                 "       execute immediate '" +
                 "       begin " +
-                "           :x := case ' || dbms_assert.valid_sql_name( ? ) || '() is of (ut_output_reporter_base) when true then 1 else 0 end;" +
+                "           :x := case ' || dbms_assert.simple_sql_name( ? ) || '() is of (ut_output_reporter_base) when true then 1 else 0 end;" +
                 "       end;'" +
                 "       using out l_result;" +
                 "   end;" +
