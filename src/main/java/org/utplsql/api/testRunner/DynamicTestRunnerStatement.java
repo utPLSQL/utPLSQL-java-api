6100f3c3f88779f7e537baf7c7d51b271c86cf83
pesse <derpesse@gmail.com>
2019-07-22 22:56:47 +0200

    Slow approach towards new DynamicTestRunner-Statement

--- src/main/java/org/utplsql/api/testRunner/DynamicTestRunnerStatement.java
+++ src/main/java/org/utplsql/api/testRunner/DynamicTestRunnerStatement.java
@@ -1,6 +1,10 @@
 package org.utplsql.api.testRunner;
 
+import oracle.jdbc.OracleConnection;
+import org.utplsql.api.CustomTypes;
+import org.utplsql.api.TestRunnerOptions;
 import org.utplsql.api.Version;
+import org.utplsql.api.db.DynamicParameterList;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -8,28 +12,74 @@ import java.sql.SQLException;
 
 public class DynamicTestRunnerStatement implements TestRunnerStatement {
 
-    private CallableStatement callableStatement;
-    private final Connection connection;
+    private CallableStatement stmt;
+    private final OracleConnection oracleConnection;
     private final Version utPlSQlVersion;
+    private final TestRunnerOptions options;
+    private final DynamicParameterList dynamicParameterList;
 
-    private DynamicTestRunnerStatement( Version utPlSQlVersion, Connection connection ) {
+    private DynamicTestRunnerStatement( Version utPlSQlVersion, OracleConnection connection, TestRunnerOptions options, CallableStatement statement ) throws SQLException {
         this.utPlSQlVersion = utPlSQlVersion;
-        this.connection = connection;
+        this.oracleConnection = connection;
+        this.options = options;
+        this.stmt = statement;
+
+        this.dynamicParameterList = initParameterList();
+
+        prepareStatement();
+    }
+
+    private DynamicParameterList initParameterList() throws SQLException {
+        /*
+        "BEGIN " +
+                        "ut_runner.run(" +
+                        "a_paths                  => ?, " +
+                        "a_reporters              => ?, " +
+                        "a_color_console          => " + colorConsoleStr + ", " +
+                        "a_coverage_schemes       => ?, " +
+                        "a_source_file_mappings   => ?, " +
+                        "a_test_file_mappings     => ?, " +
+                        "a_include_objects        => ?, " +
+                        "a_exclude_objects        => ?, " +
+                        "a_fail_on_errors         => " + failOnErrors + ", " +
+                        "a_client_character_set   => ?, " +
+                        "a_random_test_order      => " + randomExecutionOrder + ", " +
+                        "a_random_test_order_seed => ?, "+
+                        "a_tags => ?"+
+                        "); " +
+                        "END;";
+         */
+        return DynamicParameterList.builder()
+                .addIfNotEmpty("a_paths", options.pathList.toArray(), CustomTypes.UT_VARCHAR2_LIST, oracleConnection)
+                .build();
+    }
+
+    private void prepareStatement() throws SQLException {
+        if ( stmt == null )
+            oracleConnection.prepareCall(dynamicParameterList.getSql());
+
+        dynamicParameterList.setParamsStartWithIndex(stmt, 1);
     }
 
     @Override
     public void execute() throws SQLException {
+
         // Implement
     }
 
     @Override
+    public String getSql() {
+        return dynamicParameterList.getSql();
+    }
+
+    @Override
     public void close() throws SQLException {
-        if (callableStatement != null) {
-            callableStatement.close();
+        if (stmt != null) {
+            stmt.close();
         }
     }
 
-    public static DynamicTestRunnerStatement forVersion(Version version, Connection connection) {
-        return new DynamicTestRunnerStatement(version, connection);
+    public static DynamicTestRunnerStatement forVersion(Version version, OracleConnection connection, TestRunnerOptions options, CallableStatement statement ) throws SQLException {
+        return new DynamicTestRunnerStatement(version, connection, options, statement);
     }
 }
--- src/test/java/org/utplsql/api/testRunner/DynamicTestRunnerStatementTest.java
+++ src/test/java/org/utplsql/api/testRunner/DynamicTestRunnerStatementTest.java
@@ -1,12 +1,36 @@
 package org.utplsql.api.testRunner;
 
+import oracle.jdbc.OracleConnection;
 import org.junit.jupiter.api.Test;
+import org.utplsql.api.CustomTypes;
+import org.utplsql.api.TestRunnerOptions;
 import org.utplsql.api.Version;
+
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class DynamicTestRunnerStatementTest {
 
     @Test
-    void explore() {
-        DynamicTestRunnerStatement testRunnerStatement = DynamicTestRunnerStatement.forVersion(Version.V3_1_7, null);
+    void explore() throws SQLException {
+
+        OracleConnection oracleConnection = mock(OracleConnection.class);
+        CallableStatement callableStatement = mock(CallableStatement.class);
+
+        TestRunnerOptions options = TestRunnerStatementProviderIT.getCompletelyFilledOptions();
+
+        DynamicTestRunnerStatement testRunnerStatement = DynamicTestRunnerStatement
+                .forVersion(Version.V3_1_7, oracleConnection, options, callableStatement);
+
+        assertThat(testRunnerStatement.getSql(), containsString("a_paths => ?"));
+
+        verify(callableStatement).setArray(1, null);
+        verify(oracleConnection).createOracleArray(CustomTypes.UT_VARCHAR2_LIST, options.pathList.toArray());
     }
 }
