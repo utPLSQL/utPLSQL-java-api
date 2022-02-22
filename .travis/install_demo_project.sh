4bef1edd653a108fe5f92d0368d04d5b70bf3b9a
Jacek Gebal <jgebal@users.noreply.github.com>
2022-02-23 01:54:06 +0200

    Fixing build process after updates to demo-project

--- .travis/install_demo_project.sh
+++ .travis/install_demo_project.sh
@@ -17,9 +17,13 @@ cd /${PROJECT_FILE}
 sqlplus -S -L ${DB_USER}/${DB_PASS}@//127.0.0.1:1521/xe <<SQL
 whenever sqlerror exit failure rollback
 whenever oserror  exit failure rollback
-set echo on
-
 @source/install.sql
+exit
+SQL
+
+sqlplus -S -L ${DB_USER}/${DB_PASS}@//127.0.0.1:1521/xe <<SQL
+whenever sqlerror exit failure rollback
+whenever oserror  exit failure rollback
 @test/install.sql
 exit
 SQL
