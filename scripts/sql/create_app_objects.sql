2b13d6d34ab992dea52dd3b65899832ebafb90cd
Simon Martinelli <simon@72.services>
2023-07-10 17:15:18 +0200

    Added test scripts

--- scripts/sql/create_app_objects.sql
+++ scripts/sql/create_app_objects.sql
@@ -1,9 +1,9 @@
 whenever sqlerror exit failure rollback
 whenever oserror  exit failure rollback
 
-@scripts/sql/TO_TEST_ME.sql
-@scripts/sql/APP.PKG_TEST_ME.pks
-@scripts/sql/APP.PKG_TEST_ME.pkb
+@scripts/sql/simple/scripts/sources/TO_TEST_ME.sql
+@scripts/sql/simple/scripts/sources/APP.PKG_TEST_ME.pks
+@scripts/sql/simple/scripts/sources/APP.PKG_TEST_ME.pkb
 
-@scripts/sql/APP.TEST_PKG_TEST_ME.pks
-@scripts/sql/APP.TEST_PKG_TEST_ME.pkb
+@scripts/sql/simple/scripts/tests/APP.TEST_PKG_TEST_ME.pks
+@scripts/sql/simple/scripts/tests/APP.TEST_PKG_TEST_ME.pkb
