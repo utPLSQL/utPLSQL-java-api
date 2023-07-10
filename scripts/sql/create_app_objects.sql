ad79d5925fa21911269d21e027e9934e981959db
Simon Martinelli <simon@72.services>
2023-07-10 17:02:32 +0200

    Added test files

--- scripts/sql/create_app_objects.sql
+++ scripts/sql/create_app_objects.sql
@@ -1,9 +1,9 @@
 whenever sqlerror exit failure rollback
 whenever oserror  exit failure rollback
 
-@src/test/resources-its/org/utplsql/maven/plugin/UtPlsqlMojoIT/simple/scripts/sources/TO_TEST_ME.sql
-@src/test/resources-its/org/utplsql/maven/plugin/UtPlsqlMojoIT/simple/scripts/sources/APP.PKG_TEST_ME.pks
-@src/test/resources-its/org/utplsql/maven/plugin/UtPlsqlMojoIT/simple/scripts/sources/APP.PKG_TEST_ME.pkb
+@scripts/sql/TO_TEST_ME.sql
+@scripts/sql/APP.PKG_TEST_ME.pks
+@scripts/sql/APP.PKG_TEST_ME.pkb
 
-@src/test/resources-its/org/utplsql/maven/plugin/UtPlsqlMojoIT/simple/scripts/tests/APP.TEST_PKG_TEST_ME.pks
-@src/test/resources-its/org/utplsql/maven/plugin/UtPlsqlMojoIT/simple/scripts/tests/APP.TEST_PKG_TEST_ME.pkb
+@scripts/sql/APP.TEST_PKG_TEST_ME.pks
+@scripts/sql/APP.TEST_PKG_TEST_ME.pkb
